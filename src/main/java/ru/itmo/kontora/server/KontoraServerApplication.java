ccf1e2d0097318ce11593512693afeaa74389faa
ruaglsc <stanislav.ugolnikov@raiffeisen.ru>
2022-12-11 20:52:24 +0300

    Initial commit

---
+++ src/main/java/ru/itmo/kontora/server/KontoraServerApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.kontora.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontoraServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KontoraServerApplication.class, args);
+    }
+
+}
