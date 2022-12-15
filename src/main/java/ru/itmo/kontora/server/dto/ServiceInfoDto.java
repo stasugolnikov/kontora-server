a442111ab1ae7a7f3dd7dc6058776616a40cb897
ruaabzw <aleksey.moskvinov@raiffeisen.ru>
2022-12-15 21:24:19 +0300

    govno

--- src/main/java/ru/itmo/kontora/server/dto/ServiceInfoDto.java
+++ src/main/java/ru/itmo/kontora/server/dto/ServiceInfoDto.java
@@ -1,5 +1,10 @@
 package ru.itmo.kontora.server.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class ServiceInfoDto {
     private Long id;
     private Long price;
